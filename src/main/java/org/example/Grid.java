1db7a56f664252994b602a6acdf527cce2f94160
23lucas214 <147158923+23lucas214@users.noreply.github.com>
2024-01-18 08:53:26 +0000

    Update Grid.java

--- src/main/java/org/example/Grid.java
+++ src/main/java/org/example/Grid.java
@@ -16,14 +16,40 @@ public class Grid {
         return matrix[point.getY()][point.getX()];
     }
 
-    void AjouterNavire(Navire navire, Point point, int orientation){
+    void ajouterNavire(Navire navire, Point point, int orientation) throws Exception{
         int sens = -1;
         int x = point.getX();
         int y = point.getY();
         if (orientation<3){sens=1;}
         for (int i=0; i< navire.getSize(); i++){
+            if (orientation%2 == 0){
+                if(matrix[y+sens*i][x]!=null){throw new Exception("Collision navire");}
+            }
+        
+            else {
+                if(matrix[y][x+sens*i]!=null){throw new Exception("Collision navire");}
+            }
+        }
+        for (int i=0; i< navire.getSize(); i++){
             if (orientation%2 == 0){matrix[y+sens*i][x] = navire;}
             else {matrix[y][x+sens*i] = navire;}
         }
+    }
+    
+    public boolean verifierNavire(int size, int x, int y, int orientation){
+        boolean estdedans = true;
+        if ((orientation == 1) && (y+size-1>5)){
+            estdedans=false;
+        }
+        else if ((orientation == 2) && (x+size-1>5)){
+            estdedans=false;
+        }
+        else if ((orientation == 3) && (y-size<0)){
+            estdedans=false;
+        }
+        else if ((orientation == 4) && (x-size<0)){
+            estdedans=false;
+        }
+        return estdedans;
     }
 }
