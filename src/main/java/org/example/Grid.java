ec29b23cd87a6ab88f166ec92f490710300b8197
23lucas214 <147158923+23lucas214@users.noreply.github.com>
2024-01-18 09:01:15 +0000

    Update Grid.java

--- src/main/java/org/example/Grid.java
+++ src/main/java/org/example/Grid.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Grid {
     private int size = 5;
@@ -51,5 +52,90 @@ public class Grid {
             estdedans=false;
         }
         return estdedans;
+    }
+    
+    
+        public void initializeGrid(){
+        boolean done=false;
+        int x;
+        int y;
+        int o;
+        Scanner sc = new Scanner(System.in);
+        while (done!=true){
+        done=true;
+        System.out.println("Où voulez-vous placer le porte-avion ? (entrez x puis y puis l'orientation)");
+        x = sc.nextInt();
+        y = sc.nextInt();
+        System.out.println("Orientation: \n 1:haut \n 2:droite \n 3:bas \n 4:gauche");
+        o = sc.nextInt();
+        Point p = new Point(x,y);
+        Navire n = new Navire (5);
+        if (this.verifierNavire(5,x,y,o)){
+            try {
+                this.ajouterNavire(n, p, o);
+            }
+            catch (Exception e){
+                System.out.println("Collision");
+                done=false;
+            }
+        }
+        else {
+            System.out.println("En dehors de la grille");
+            done=false;
+        }
+        }
+        
+        
+        done=false;
+        while (done!=true){
+        done=true;
+        System.out.println("Où voulez-vous placer le cuirassé ? (entrez x puis y puis l'orientation)");
+        x = sc.nextInt();
+        y = sc.nextInt();
+        System.out.println("Orientation: \n 1:haut \n 2:droite \n 3:bas \n 4:gauche");
+        o = sc.nextInt();
+        Point p = new Point(x,y);
+        Navire n = new Navire (5);
+        if (this.verifierNavire(4,x,y,o)){
+            try {
+                this.ajouterNavire(n, p, o);
+            }
+            catch (Exception e){
+                System.out.println("Collision");
+                done=false;
+            }
+        }
+        else {
+            System.out.println("En dehors de la grille");
+            done=false;
+        }
+        }
+        
+        
+        done=false;
+        while (done!=true){
+        done=true;
+        System.out.println("Où voulez-vous placer le destroyer ? (entrez x puis y puis l'orientation)");
+        x = sc.nextInt();
+        y = sc.nextInt();
+        System.out.println("Orientation: \n 1:haut \n 2:droite \n 3:bas \n 4:gauche");
+        o = sc.nextInt();
+        Point p = new Point(x,y);
+        Navire n = new Navire (5);
+        if (this.verifierNavire(3,x,y,o)){
+            try {
+                this.ajouterNavire(n, p, o);
+            }
+            catch (Exception e){
+                System.out.println("Collision");
+                done=false;
+            }
+        }
+        else {
+            System.out.println("En dehors de la grille");
+            done=false;
+        }
+        }
+        
     }
 }
